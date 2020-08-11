791b0b8306f9a6dd403baa3a8125b58b40327701
Yon Liud <45634836+YonLiud@users.noreply.github.com>
2020-08-12 00:12:22 +0300

    init commit

---
+++ Client/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace Client
+{
+    public class Client : BaseScript
+    {
+        public Client()
+        {
+            EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
+        }
+        private void OnClientResourceStart(string resourceName)
+        {
+
+        }
+    }
+}
